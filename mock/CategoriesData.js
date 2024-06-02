d3e7f61660909f9b14017cad1e4e189764d20c5a
Albert-Andrews <aaaquaye001@st.ug.edu.gh>
2024-06-02 03:08:32 +0000

    completed

---
+++ mock/CategoriesData.js
@@ -0,0 +1,50 @@
+export const CategoriesData = [
+  {
+    id: 0,
+    name: "Exercise",
+    description: "12 Task",
+    image: require("../assets/woman1.png"),
+  },
+  {
+    id: 1,
+    name: "Study",
+    description: "12 Task",
+    image: require("../assets/woman.png"),
+  },
+  {
+    id: 2,
+    name: "Work",
+    description: "12 Task",
+    image: require("../assets/sad.png"),
+  },
+  {
+    id: 3,
+    name: "Food",
+    description: "12 Task",
+    image: require("../assets/food.png"),
+  },
+  {
+    id: 4,
+    name: "Marketing",
+    description: "12 Task",
+    image: require("../assets/market.png"),
+  },
+  {
+    id: 5,
+    name: "Learning",
+    description: "12 Task",
+    image: require("../assets/learning.png"),
+  },
+  {
+    id: 6,
+    name: "Maintenance",
+    description: "12 Task",
+    image: require("../assets/maintenance.png"),
+  },
+  {
+    id: 7,
+    name: "Inventory",
+    description: "12 Task",
+    image: require("../assets/inventory.png"),
+  },
+];
---
+++ mock/taskData.js
@@ -0,0 +1,66 @@
+export const taskData = [
+  {
+    id: 0,
+    name: "Mobile App Development",
+  },
+  {
+    id: 1,
+    name: "Web Devleopment",
+  },
+  {
+    id: 2,
+    name: "Push Ups",
+  },
+  {
+    id: 3,
+    name: "Push Ups",
+  },
+  {
+    id: 4,
+    name: "Respond to Client Emails",
+  },
+  {
+    id: 5,
+    name: "Update Project Management Software ",
+  },
+  {
+    id: 6,
+    name: "Prepare for Weekly Team Meeting",
+  },
+  {
+    id: 7,
+    name: "Review Financial Reports ",
+  },
+  {
+    id: 8,
+    name: "Conduct Market Research ",
+  },
+  {
+    id: 9,
+    name: "Develop Marketing Strategy",
+  },
+  {
+    id: 10,
+    name: "Maintain Social Media Accounts",
+  },
+  {
+    id: 11,
+    name: "Train New Employees ",
+  },
+  {
+    id: 12,
+    name: "Quality Assurance Testing ",
+  },
+  {
+    id: 13,
+    name: "Client Project Work",
+  },
+  {
+    id: 14,
+    name: "Update Website Content ",
+  },
+  {
+    id: 15,
+    name: "Network Maintenance ",
+  },
+];
